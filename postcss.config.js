4422920da4417bb30f20283be9843356a958a273
Nul Pàs <manu@lunaeme.com>
2018-11-14 14:17:16 +0100

    Medea Template System :: v.0.0.0-beta.02  · Testing font manage.

---
+++ postcss.config.js
@@ -0,0 +1,35 @@
+const fontPathFile = '../fonts/nunito-v9-latin-regular.';
+
+module.exports = {
+  plugins: {
+    'postcss-font-magician': {
+      custom: {
+        'Nunito': {
+          variants: {
+            normal: {
+              400: {
+                url: {
+                  woff2: fontPathFile + 'woff2',
+                  woff: fontPathFile + 'woff',
+                }
+              },
+              700: {
+                url: {
+                  woff2: fontPathFile + 'woff2',
+                }
+              }
+            },
+            italic: {
+              400: {
+                url: {
+                  woff2: fontPathFile + 'woff2',
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    'postcss-cssnext': {}
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    __dirname + '/src/style/index.scss'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'medea.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'resolve-url-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/i,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/i,
+      //   loader: 'file-loader?name=assets/img/[name].[ext]'
+      // }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'medea.css'
+    })
+  ]
+};
