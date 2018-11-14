bec35b9df1d3bbad14b5352d47cd302a0fa52387
Nul Pàs <manu@lunaeme.com>
2018-11-14 19:53:42 +0100

    Medea Template System :: v.0.0.0-beta.07  · Testing font-faces.

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,5 @@
-const fontPathFile = '../fonts/nunito-v9-latin-regular.';
-const fontUrl = {
-  eot: fontPathFile + 'eot',
-  svg: fontPathFile + 'svg',
-  ttf: fontPathFile + 'ttf',
-  woff: fontPathFile + 'woff',
-  woff2: fontPathFile + 'woff2'
-};
-
 module.exports = {
-  plugins: {
-    'postcss-font-magician': {
-      custom: {
-        'Nunito': {
-          variants: {
-            normal: {
-              600: { url: fontUrl }
-            }
-          }
-        }
-      }
-    },
-    'postcss-cssnext': {}
-  }
+  plugins: [
+    require('postcss-cssnext')
+  ]
 };
